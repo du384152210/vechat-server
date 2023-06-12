b80fd1f2fca5bbc52def7c933d902c722f06353f
du384152210 <du384152210@163.com>
2023-06-12 18:40:50 +0800

    update

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
-  if (req.url != '/login' && req.url != '/register') {
+  if (req.url != '/login' && req.url != '/register' && req.url != '/upload') {
     let token = req.headers.authorization;
     let result = varifyToken(token);
     if (result.code === 200) {
--- router/upload/index.js
+++ router/upload/index.js
@@ -1,18 +1,21 @@
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/images')
+    cb(null, 'G:\\personal\\node\\vechat-server\\public')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1])
   }
 })
 
 const upload = multer({ storage })
 
 module.exports = (app) => {
-  app.post('/upload', upload.array('photos', 10), function (req, res, next) {
-    console.log(req,files);
+  app.post('/upload', upload.single('file'), function (req, res, next) {
+    res.json({
+      message: 'success',
+      src: req.file.filename
+    })
   })
 }
